c6cff291c67eb98cacb9450450537fc07642ccc2
christophertorres0 <31553679+christophertorres0@users.noreply.github.com>
2019-02-21 19:16:10 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function nextImage(el){
+
+if (el.src.match("Images/Goblin.PNG")){
+
+el.src = "Images/prowler.gif";
+
+// if the image is a cherry, change it to a lemon!
+
+} else if (el.src.match("Images/prowler.gif")){
+
+el.src = "Images/kingpen.PNG";
+
+// if the image is a lemon, change it to an orange!
+
+} else if (el.src.match("Images/kingpen.PNG")){
+
+el.src = "Images/Goblin.PNG";
+
+// if the image is an orange, change it to a cherry!
+
+} else {
+
+// do nothing
+
+}
